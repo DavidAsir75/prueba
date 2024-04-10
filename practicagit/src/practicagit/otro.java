b4bd7562fd54811d6303eebbfcbf860e1c71905d
DavidAsir75 <166594486+DavidAsir75@users.noreply.github.com>
2024-04-10 20:38:50 +0200

    Update otro.java

--- practicagit/src/practicagit/otro.java
+++ practicagit/src/practicagit/otro.java
@@ -12,7 +12,7 @@ public class otro {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO lo no cambies Auto-generated method stub
+		// ha vuelto a cambir cambies Auto-generated method stub
 
 	}
 
