fe4a0c770e4c4e67c7dedc7f52d47a11a13c4555
DavidAsir75 <domedes@iespabloserrano.com>
2024-04-11 10:10:26 +0200

    mnbm

--- practicagit/src/practicagit/otro.java
+++ practicagit/src/practicagit/otro.java
@@ -13,8 +13,7 @@ public class otro {
 	 */
 	public static void main(String[] args) {
 		// ha vuelto a cambir cambies Auto-generated method stub
-		// segundo cambo
-		// no me gusta
+				// no me gusta
 		//tercer cambio
 		//cuarto cambio
 	}
